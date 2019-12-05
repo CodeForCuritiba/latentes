daa594cf550af21c07e7d24ece4669b520a293da
Jonhnatha Trigueiro <joepreludian@gmail.com>
2019-12-05 00:19:50 -0300

    Add a simple test case to State

--- backend/base/tests.py
+++ backend/base/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from base.models import City, State
 
-# Create your tests here.
+
+class StateTestCase(TestCase):
+    def setUp(self):
+        State.objects.create(initials='PB')
+
+    def test_state_exists(self):
+        """Validate simple add of a State"""
+        pb = State.objects.get(initials='PB')
+
+        self.assertEqual(str(pb), 'PB')
